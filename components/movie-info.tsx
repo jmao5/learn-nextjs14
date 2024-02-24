a670b549b6a6188f924eb7db48cb48f4b6481d66
jmao5 <jsh5890@kakao.com>
2024-02-24 23:23:00 +0900

    cssㅅㄱ

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,4 +1,5 @@
 import { API_URL } from "@/constants/url";
+import styles from "@/styles/movie-info.module.css";
 
 const getMovie = async (id: number) => {
   const res = await fetch(`${API_URL}/${id}`);
@@ -7,5 +8,21 @@ const getMovie = async (id: number) => {
 
 export default async function MovieInfo({ id }: { id: number }) {
   const movie = await getMovie(id);
-  return <h6>{JSON.stringify(movie)}</h6>;
+  return (
+    <div className={styles.container}>
+      <img
+        src={movie.poster_path}
+        alt={movie.title}
+        className={styles.poster}
+      />
+      <div className={styles.info}>
+        <h1 className={styles.title}>{movie.title}</h1>
+        <h3>⭐{movie.vote_average}</h3>
+        <p>{movie.overview}</p>
+        <a href={movie.homepage} target={"_blank"}>
+          Homepage &rarr;
+        </a>
+      </div>
+    </div>
+  );
 }
--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,4 +1,5 @@
 import { API_URL } from "@/constants/url";
+import styles from "@/styles/movie-videos.module.css";
 
 const getVideos = async (id: number) => {
   const res = await fetch(`${API_URL}/${id}/videos`);
@@ -6,6 +7,18 @@ const getVideos = async (id: number) => {
 };
 
 export default async function MovieVideos({ id }: { id: number }) {
-  const video = await getVideos(id);
-  return <h6>{JSON.stringify(video)}</h6>;
+  const videos = await getVideos(id);
+  return (
+    <div className={styles.container}>
+      {videos.map((video) => (
+        <iframe
+          key={video.id}
+          src={`https://youtube.com/embed/${video.key}`}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          title={video.name}
+        />
+      ))}
+    </div>
+  );
 }
