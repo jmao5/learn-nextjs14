631d7b7c6ef60d8cdc815cb42ac4618985dbfa8a
jmao5 <jsh5890@kakao.com>
2024-02-24 22:41:39 +0900

    링크까지

--- components/movie.tsx
+++ components/movie.tsx
@@ -1,10 +1,17 @@
+"use client";
+
 import Link from "next/link";
 import styles from "@/styles/movie.module.css";
+import { useRouter } from "next/navigation";
 
 export default function Movie({ title, id, poster_path }) {
+  const router = useRouter();
+  const linkHandler = () => {
+    router.push(`movies/${id}`);
+  };
   return (
     <div key={id} className={styles.movie}>
-      <img src={poster_path} alt={title} />
+      <img src={poster_path} alt={title} onClick={linkHandler} />
       <Link href={`movies/${id}`}>{title}</Link>
     </div>
   );
